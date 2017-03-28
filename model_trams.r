2597b8d22b92c226a4f5fc1a62186816f4738f35
Olli-Pekka Huovilainen <olli-pekka.huovilainen@reaktor.com>
2017-03-28 15:21:43 +0300

    Added variance model to parallelisation

--- model_trams.r
+++ model_trams.r
@@ -27,14 +27,19 @@ res_model_list <- list()
 model_info <- data.frame(id = numeric(), line = character(), dir = numeric())
 
 
-models <- foreach(v_line = lines) %do% {
+models <- foreach(v_line = rep(lines,each=2),
+                  v_dir = rep(1:2,times=length(lines))) %do% {
   md_leg = leg_times_trams %>% 
     filter(desi == v_line, dir == 1, !is.na(model_hour), !is.na(weekday)) %>% 
     select(legTime, nextStop, weekday, model_hour)
   if (nrow(md_leg) > 200) {
-    gam(legTime ~ factor(nextStop) + factor(weekday) + s(model_hour,by=factor(weekday)), 
+    gam_model <- gam(legTime ~ factor(nextStop) + factor(weekday) + s(model_hour,by=factor(weekday)), 
         family = gaussian(link = "identity"), 
         data = md_leg)
+    gam_res_model <- gam(res ~ factor(nextStop) + factor(weekday) + s(model_hour,by=factor(weekday)), family = Gamma(link = "log"),
+                         data = md_leg %>%
+                           mutate(res = abs(legTime - predict(gam_model))))
+    list(avg_model = gam_model, var_model = gam_res_model)
   }
 }
 
@@ -43,17 +48,17 @@ models <- foreach(v_line = lines) %do% {
 # for (v_line in lines){
 #   for (v_dir in 1:2){
 #     print(str_c("line: ", v_line, " dir: ", v_dir))
-#     md_leg = leg_times_trams %>% 
-#       filter(desi == v_line, dir == v_dir, !is.na(model_hour), !is.na(weekday)) %>% 
+#     md_leg = leg_times_trams %>%
+#       filter(desi == v_line, dir == v_dir, !is.na(model_hour), !is.na(weekday)) %>%
 #       select(legTime, nextStop, weekday, model_hour)
 #     if (nrow(md_leg) < 200) next
 #     print("Fitting leg time model...")
-#     gam_model <- gam(legTime ~ factor(nextStop) + factor(weekday) + s(model_hour,by=factor(weekday)), 
-#                      family = gaussian(link = "identity"), 
+#     gam_model <- gam(legTime ~ factor(nextStop) + factor(weekday) + s(model_hour,by=factor(weekday)),
+#                      family = gaussian(link = "identity"),
 #                      data = md_leg)
 #     print("Fitting variance model...")
 #     gam_res_model <- gam(res ~ factor(nextStop) + factor(weekday) + s(model_hour,by=factor(weekday)), family = Gamma(link = "log"),
-#                          data = md_leg %>% 
+#                          data = md_leg %>%
 #                            mutate(res = abs(legTime - predict(gam_model))))
 #     model_id <- str_c("L",v_line,v_dir)
 #     model_list[[model_id]] <- gam_model
